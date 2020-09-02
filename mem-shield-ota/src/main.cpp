da9679b2cb6f577093ce3fbda31fe3af77ec3737
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-09-03 00:39:26 +0200

    Just getting started

--- mem-shield-ota/src/main.cpp
+++ mem-shield-ota/src/main.cpp
@@ -1,15 +1,23 @@
 #include <Arduino.h>
 #include <MKRGSM.h>
+#include "secrets.h"
 
 GSM gsm;
+GPRS gprs;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+
+    gsm.begin(SECRET_PINNUMBER);
+    gprs.attachGPRS(SECRET_GPRS_APN, SECRET_GPRS_LOGIN, SECRET_GPRS_PASSWORD);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    yield();
 }
